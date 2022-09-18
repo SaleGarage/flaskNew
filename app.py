76c0e4e40a608a2eb3ad5f9b6283e7c8592899f9
Mig <r121221@yandex.ru>
2022-09-18 18:53:21 +0300

    добавил base

--- app.py
+++ app.py
@@ -3,18 +3,18 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/kuku/')
 def hi():  # put application's code here
     return 'sdfsdf!'
 
-
+@app.route('/')
 @app.route('/index/')
 def index():  # put application's code here
     car = {'name': ('bugatty',
 
                     'https://libertycity.ru/uploads/download/gta5_bugatti/fulls/j4q9k776k31rt5p2jnd2823s63/15043684584016_f61541-1.jpg')}
 
-    return render_template('index.html', name = car['name'][0], foto = car['name'][1])
+    return render_template('index.html', name = car['name'][0], foto = car['name'][1], title = '1' )
 
 
 @app.route('/petya/')

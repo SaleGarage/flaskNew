5c740d97e6f33000e21bf50d091be87aceca5192
user1 <ghz.tormyshev@yandex.ru>
2023-01-29 19:07:36 +0300

    54

--- app.py
+++ app.py
@@ -49,8 +49,8 @@ def login():  # put application's code here
     form = LoginForm()
     return render_template('login.html', title='Авторизация пользователя', form=form)
 
-@app.route('/login4')
-def login4():  # put application's code here
+@app.route('/login5')
+def login5():  # put application's code here
     form = LoginForm()
     return render_template('login.html', title='Авторизация пользователя', form=form)
 
